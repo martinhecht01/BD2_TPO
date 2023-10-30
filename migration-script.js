0e996a1b9880ac7540358e13d51c923c6c16ff77
Martin Hexht <mhecht@itba.edu.ar>
2023-10-29 21:40:57 -0300

    feat: migration scripts

---
+++ migration-script.js
@@ -0,0 +1,11 @@
+// This is a migration script which did not work and only remains in the repo as a 
+// reference in case we want to continue working on this line
+
+
+// use E03;
+// db.createUser({user: "username3", pwd: "password", roles: [ { role: "readWrite", db: "E03" } ]});
+// db.auth("username3", "password");
+// mongoimport --db E01 --collection phones --jsonArray --file /phones.json;
+// mongoimport --db E01 --collection tickets --jsonArray --file /tickets.json;
+// mongoimport --db E01 --collection clients --jsonArray --file /clients.json;
+// mongoimport --db E01 --collection products --jsonArray --file /products.json;
--- migration.sh
+++ migration.sh
@@ -1,19 +1,21 @@
-#./migration.sh
+#!/bin/bash
+
 #docker pull mongo
 #docker run -d -p 27017:27017 --name mongodb mongo
-# docker cp ./tickets.json mongodb:/
-# docker cp ./clients.json mongodb:/
-# docker cp ./phones.json mongodb:/
-# docker cp ./products.json mongodb:/
-# docker exec -it mongodb bash
-mongosh
-use E01
-db.createUser({user: "username2", pwd: "password",roles: [ { role: "readWrite", db: "E01" } ]})
-db.auth("username2", "password")
-exit
-mongoimport --db E01 --collection phones --jsonArray --file phones.json
-mongoimport --db E01 --collection tickets --jsonArray --file tickets.json
-mongoimport --db E01 --collection clients --jsonArray --file clients.json
-mongoimport --db E01 --collection products --jsonArray --file products.json
+
+# Copy JSON files into the MongoDB container
+docker cp ./tickets.json mongodb:/
+docker cp ./clients.json mongodb:/
+docker cp ./phones.json mongodb:/
+docker cp ./products.json mongodb:/
+
+# Execute MongoDB commands in the MongoDB container
+docker cp ./migration-script.js mongodb:/
+docker exec -it mongodb bash -c 'mongosh -f ./migration-script.js'
+
+# Remove the MongoDB container after the import
+# docker stop mongodb
+# docker rm mongodb
+
 
 
