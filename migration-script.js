5dfe4bdce381256c1777d9b851442bb8cb7e5775
Martin Hexht <mhecht@itba.edu.ar>
2023-10-29 21:55:21 -0300

    fix: migration file

--- migration-script.js
+++ migration-script.js
@@ -1,11 +1,7 @@
-// This is a migration script which did not work and only remains in the repo as a 
-// reference in case we want to continue working on this line
-
-
-// use E03;
-// db.createUser({user: "username3", pwd: "password", roles: [ { role: "readWrite", db: "E03" } ]});
-// db.auth("username3", "password");
-// mongoimport --db E01 --collection phones --jsonArray --file /phones.json;
-// mongoimport --db E01 --collection tickets --jsonArray --file /tickets.json;
-// mongoimport --db E01 --collection clients --jsonArray --file /clients.json;
-// mongoimport --db E01 --collection products --jsonArray --file /products.json;
+use E01;
+db.createUser({user: "username2", pwd: "password", roles: [ { role: "readWrite", db: "E01" } ]});
+db.auth("username2", "password");
+mongoimport --db E01 --collection phones --jsonArray --file /phones.json;
+mongoimport --db E01 --collection tickets --jsonArray --file /tickets.json;
+mongoimport --db E01 --collection clients --jsonArray --file /clients.json;
+mongoimport --db E01 --collection products --jsonArray --file /products.json;
--- migration.sh
+++ migration.sh
@@ -11,11 +11,17 @@ docker cp ./products.json mongodb:/
 
 # Execute MongoDB commands in the MongoDB container
 docker cp ./migration-script.js mongodb:/
-docker exec -it mongodb bash -c 'mongosh -f ./migration-script.js'
+docker exec -it mongodb bash
+mongosh
+use E01
+db.createUser({user: "username2", pwd: "password",roles: [ { role: "readWrite", db: "E01" } ]})
+db.auth("username2", "password")
+exit
+mongoimport --db E01 --collection phones --jsonArray --file phones.json
+mongoimport --db E01 --collection tickets --jsonArray --file tickets.json
+mongoimport --db E01 --collection clients --jsonArray --file clients.json
+mongoimport --db E01 --collection products --jsonArray --file products.json
 
 # Remove the MongoDB container after the import
 # docker stop mongodb
 # docker rm mongodb
-
-
-
